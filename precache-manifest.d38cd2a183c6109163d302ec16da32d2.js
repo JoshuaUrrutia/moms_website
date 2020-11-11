a7fe2ccd691b822315f060f9ed650c9f32b34ff9
urrutia <jurrutia@tacc.utexas.edu>
2020-11-10 23:36:49 -0600

    Updates

---
+++ precache-manifest.d38cd2a183c6109163d302ec16da32d2.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "24ea651e9646081bd52e",
+    "url": "/moms_website/static/css/main.8f08483d.chunk.css"
+  },
+  {
+    "revision": "24ea651e9646081bd52e",
+    "url": "/moms_website/static/js/main.cbb8091d.chunk.js"
+  },
+  {
+    "revision": "39f875ff4ed3c4340186",
+    "url": "/moms_website/static/js/runtime~main.7d74a3f2.js"
+  },
+  {
+    "revision": "694fe315a5631896ffe9",
+    "url": "/moms_website/static/css/2.55c7edcb.chunk.css"
+  },
+  {
+    "revision": "694fe315a5631896ffe9",
+    "url": "/moms_website/static/js/2.fecbadfc.chunk.js"
+  },
+  {
+    "revision": "2923fb84801cf4405af1216a1438f350",
+    "url": "/moms_website/static/media/cu4dinner.2923fb84.png"
+  },
+  {
+    "revision": "edaf05d52204482afc41bd8824adb9d6",
+    "url": "/moms_website/static/media/rest.edaf05d5.jpg"
+  },
+  {
+    "revision": "b22e2d3f586fc03f442e0728ad497523",
+    "url": "/moms_website/static/media/rest1.b22e2d3f.jpg"
+  },
+  {
+    "revision": "41c72d87dbf5e83f3ea9eac64650e1c0",
+    "url": "/moms_website/static/media/rest2.41c72d87.jpg"
+  },
+  {
+    "revision": "630fb8e683b3c5470acf8862f59b1053",
+    "url": "/moms_website/static/media/rest3.630fb8e6.jpg"
+  },
+  {
+    "revision": "a1a74195064c2a215dbf4ec40c4a24b7",
+    "url": "/moms_website/static/media/vlad.a1a74195.jpg"
+  },
+  {
+    "revision": "a1a74195064c2a215dbf4ec40c4a24b7",
+    "url": "/moms_website/static/media/vlad-img.a1a74195.jpg"
+  },
+  {
+    "revision": "090373ca574bdf420886fc59bad86bb5",
+    "url": "/moms_website/static/media/offerpic.090373ca.jpg"
+  },
+  {
+    "revision": "4209510857dc47a354e5cc576696aba0",
+    "url": "/moms_website/static/media/contact_us.42095108.jpg"
+  },
+  {
+    "revision": "9f65e41d8b9b8177ce30ddaf5906d67c",
+    "url": "/moms_website/static/media/offerpic2.9f65e41d.jpeg"
+  },
+  {
+    "revision": "53ad6e7a14337af6f93c5a406eb8db2a",
+    "url": "/moms_website/static/media/green_burger.53ad6e7a.jpg"
+  },
+  {
+    "revision": "94a580f6e399b54edd9f712148a8af8f",
+    "url": "/moms_website/static/media/offerpic3.94a580f6.jpg"
+  },
+  {
+    "revision": "22edf3cb5c17747dd57742e858db1362",
+    "url": "/moms_website/static/media/birthday.22edf3cb.jpg"
+  },
+  {
+    "revision": "be4d610ea1f57d8ffe7f36c878ef121e",
+    "url": "/moms_website/static/media/pizzaParty.be4d610e.jpg"
+  },
+  {
+    "revision": "185f5aa44cb78cdf200d1a780d829d3e",
+    "url": "/moms_website/static/media/jay.185f5aa4.jpg"
+  },
+  {
+    "revision": "18d75ebc9d6bf5886d25dddde754adf0",
+    "url": "/moms_website/static/media/backPizza.18d75ebc.jpg"
+  },
+  {
+    "revision": "0579872ad9fa25d5c9785572ebebe206",
+    "url": "/moms_website/static/media/serve.0579872a.png"
+  },
+  {
+    "revision": "56cff607150157f60d312e21b61291b4",
+    "url": "/moms_website/static/media/cart.56cff607.jpg"
+  },
+  {
+    "revision": "d5ab2f42acc1ee66473332ae03dc3dd7",
+    "url": "/moms_website/static/media/offer.d5ab2f42.png"
+  },
+  {
+    "revision": "7c9e64056c844d15457c8559c2b9169e",
+    "url": "/moms_website/index.html"
+  }
+];
